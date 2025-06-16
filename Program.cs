f4b94be1fa20db9bcfe6f8416ea1de0787bb0eb7
Haitam1202 <Htam1402@gmail.com>
2025-06-16 11:45:11 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Rằm Tháng Giêng");
+        Console.WriteLine("Rằm xuân lồng lộng trăng soi,");
+        Console.WriteLine("Sông xuân nước lẫn màu trời thêm xuân.");
+        Console.WriteLine("Giữa dòng bàn bạc việc quân");
+        Console.WriteLine("Khuya về bát ngát trăng ngân đầy thuyền.");
+        Console.WriteLine("Hồ Chí Minh.");
+    }
+}
